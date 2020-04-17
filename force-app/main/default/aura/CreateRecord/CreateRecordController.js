c5eb69cabfec4c2c974c5c09d8c4e57aab4cd9f9
hrshtagg <hrshtagg@gmail.com>
2020-04-18 01:55:34 +0530

    13

---
+++ force-app/main/default/aura/CreateRecord/CreateRecordController.js
@@ -0,0 +1,25 @@
+({
+    createRecord : function (component, event, helper) {
+        var createRecordEvent = $A.get("e.force:createRecord");
+        createRecordEvent.setParams({
+            "entityApiName": "Contact",
+            "defaultFieldValues": {
+                'Phone' : '415-240-6590',
+                'AccountId' : component.get("v.recordId")
+            }
+        });
+        createRecordEvent.fire();
+    },
+    editRecord : function (component, event, helper) {
+        var editRecordEvent = $A.get("e.force:editRecord");
+        editRecordEvent.setParams({
+            "recordId": component.get("v.recordId")
+        });
+        editRecordEvent.fire();
+    },
+    handleChange: function (cmp, event) {
+        // This will contain an array of the "value" attribute of the selected options
+        var selectedOptionValue = event.getParam("value");
+        alert("Option selected with value: '" + selectedOptionValue.toString() + "'");
+    }
+})
